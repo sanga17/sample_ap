901158a4bc660166bf7cdd5a6b63b4d28b6f78bb
sanga17 <sangmesh.ausekar@wgblindia.com>
2018-02-12 13:52:48 +0530

    Finish layout and routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,6 +4,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get contact" do
     get static_pages_contact_url
     assert_response :success
+    assert_select "title", "Ruby on Rails Tutorial Sample App"
   end
 
 end
